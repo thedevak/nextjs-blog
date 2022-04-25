04b539433df6ab9c9a5a7ec3bedd4d2af752e9b5
thedevak <akthedev@outlook.com>
2022-04-25 22:07:39 +0530

    3 files modified, 1 added

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <Image src="/atom-logo-feature.webp" width={1200} height={900} />
+            <Image src="/atom-logo-feature.webp" width={600} height={450} />
             <h3>Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
